216d9ccec9c6072320d3138e5b249e20f2c545b7
marina-fdz <marina.fdz.caceres@gmail.comgit config --global core.editor code>
2024-05-09 13:28:34 +0200

    fix head

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -92,14 +92,14 @@ function App() {
         <h2 className="title">Información</h2>
         <fieldset className="addForm__group">
           <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
-          <input className="addForm__input" type="text" name="name" id="name" placeholder="Nombre del proyecto" onChange={handleChange}/>
-          <input className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Slogan" onChange={handleChange}/>
+          <input className="addForm__input" type="text" name="name" id="name" placeholder="Nombre del proyecto" onChange={handleChange} maxLength='16'/>
+          <input className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Slogan" onChange={handleChange} maxLength='18'/>
           <div className="addForm__2col">
             <input className="addForm__input" type="url" name="repo" id="repo" placeholder="Repositorio" onChange={handleChange}/>
             <input className="addForm__input" type="url" name="demo" id="demo" placeholder="Demo" onChange={handleChange}/>
           </div>         
           <input className="addForm__input" type="text" name="technologies" id="technologies" placeholder="Tecnologías" onChange={handleChange}/>
-          <textarea className="addForm__input" type="text" name="desc" id="desc" placeholder="Descripción" rows="5" onChange={handleChange}></textarea>
+          <textarea className="addForm__input textarea" type="text" name="desc" id="desc" placeholder="Descripción" rows="5" onChange={handleChange}></textarea>
         </fieldset>
     
         <fieldset className="addForm__group">

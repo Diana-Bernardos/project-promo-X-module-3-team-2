4884c4833f5a727d6b74cb0680402409818d9202
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-05-19 15:56:12 +0200

    Revert "se guardan los datos en localStorage y se cargan como state inicial"
    This reverts commit c5011361ee4218d6fb21374f0978b233a8392263.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,37 +5,37 @@ import { useState } from 'react';
 
 const Form = (props) => {
 
-  const [errorMsg, setErrorMsg] = useState({
-    name: '',
-    slogan: '',
-    technologies: '',
-    repo: '',
-    demo: '',
-    desc: '',
-    autor: '',
-    job: '',
-  });
-
+  const [errorMsg, setErrorMsg] = useState({name: "", 
+  slogan: "", 
+  technologies: "", 
+  repo: "", 
+  demo: "", 
+  desc: "", 
+  autor: "", 
+  job: "",});
 
   const validateInput = (id, inputValue) => {
     const urlPattern = /^(https?:\/\/)?([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,}(\/[a-zA-Z0-9._-]*)*\/?$/;
 
-    if (inputValue === '') {
-      setErrorMsg({...errorMsg, [id]: 'Por favor rellene este campo'})
-    } else {
-      setErrorMsg({...errorMsg, [id]: ''})
+    if(inputValue === ''){
+      setErrorMsg({...errorMsg, [id]:'Por favor rellene este campo'})
+    }else{
+      setErrorMsg({...errorMsg, [id]:''})
     }
 
-    if (id === 'repo' || id === 'demo') {
+    if(id === 'repo' || id === 'demo'){
       if (!urlPattern.test(inputValue)) {
-        setErrorMsg({...errorMsg, [id]: 'Por favor introduce una url válida'});
-      } else {
+        setErrorMsg({...errorMsg, [id]:'Por favor introduce una url válida'});
+    } else {
         setErrorMsg('');
         // Proceed with form submission or other logic
         console.log('Valid GitHub URL:', inputValue);
-      }
     }
-  };
+    }
+
+    
+      
+};
 
   const handleChange =(ev)=>{
     const id = ev.target.id;
@@ -54,8 +54,8 @@ const Form = (props) => {
           <p className='error-msg'>{errorMsg.name}</p>
           <input className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Slogan" onChange={handleChange} maxLength='30'value={props.data.slogan} required/>
           <p className='error-msg'>{errorMsg.slogan}</p>
-
           <div className="addForm__2col">
+
             <div className="addForm__url">
               <input className="addForm__input" type="url" name="repo" id="repo" placeholder="Repositorio" onChange={handleChange} value={props.data.repo} required/>
               <p className='error-msg'>{errorMsg.repo}</p>
@@ -64,8 +64,7 @@ const Form = (props) => {
               <input className="addForm__input" type="url" name="demo" id="demo" placeholder="Demo" onChange={handleChange} value={props.data.demo} required/>
               <p className='error-msg'>{errorMsg.demo}</p>
             </div>
-          </div>
-
+          </div>         
           <input className="addForm__input" type="text" name="technologies" id="technologies" placeholder="Tecnologías" onChange={handleChange} maxLength='30' value={props.data.technologies} required/>
           <p className='error-msg'>{errorMsg.technologies}</p>
           <textarea className="addForm__input textarea" type="text" name="desc" id="desc" placeholder="Descripción" rows="5" onChange={handleChange}value={props.data.desc}></textarea>
@@ -84,7 +83,8 @@ const Form = (props) => {
           <GetAvatar id="image" text="Subir foto del proyecto" updateAvatar={props.updateAvatar}/>
 
           <GetAvatar id="photo" text="Subir foto de la autora" updateAvatar={props.updateAvatar}/>
-        </fieldset>
+
+        </fieldset>  
       </form>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,42 +7,32 @@ import "../styles/Main.scss";
 
 const Main = () => {
 
-    const initialDataState = () => {
-        const localStorageData = JSON.parse(localStorage.getItem('data'))
-        if (localStorageData) {
-            return localStorageData;
-        }
+  
 
-        return {
-            name: '',
-            slogan: '',
-            technologies: '',
-            repo: '',
-            demo: '',
-            desc: '',
-            autor: '',
-            job: '',
-            image: '',
-            photo: ''
-        };
-    }
-
-    const [data, setData] = useState(initialDataState());
+    const [data, setData] = useState({
+        name: "", 
+        slogan: "", 
+        technologies: "", 
+        repo: "", 
+        demo: "", 
+        desc: "", 
+        autor: "", 
+        job: "", 
+        image: "", 
+        photo: ""
+      }
+    );
 
  
     const updateAvatar = (id, avatar) => {
       console.log(id);
-        const newAvatar = {...data, [id]: avatar};
-        setData(newAvatar);
-        localStorage.setItem('data', JSON.stringify(newAvatar));
+      setData({...data, [id]: avatar});
     };
     
     const getInput = (id, value)=>{
-        const newInput = {...data, [id]: value};
-        setData(newInput);
-        localStorage.setItem('data', JSON.stringify(newInput));
+        setData({...data, [id]: value});
     }
-
+    
     console.log(data);
 
   return (
